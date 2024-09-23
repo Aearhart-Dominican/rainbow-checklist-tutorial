fc87ab2a27b0be48aa50c6edb37eaa201becf371
Aearhart-Dominican <austin.earhart55@gmail.com>
2024-09-23 12:49:41 -0700

    created CRUD functions

---
+++ checklist.py
@@ -0,0 +1,29 @@
+checklist = []
+
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    item = checklist[index]
+    return item
+
+def update(index, item):
+    checklist[index] = item
+
+def destroy(index):
+    checklist.pop(index)
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    # print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    # print(read(1))
+
+test()
