afda550be2e6fd925f59a501630878da9b92ea52
Aearhart-Dominican <austin.earhart55@gmail.com>
2024-10-07 11:55:17 -0700

    Complete Rainbow Checklist

--- checklist.py
+++ checklist.py
@@ -13,6 +13,58 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+def select(function_code: str):
+
+    match function_code.capitalize():
+        case "C":
+            create(input("Enter Item: "))
+
+        case "R":
+            try:
+                print(read(input("Enter Index: ")))
+            except(ValueError, IndexError, TypeError):
+                print("Invalid Index Input")
+
+        case "P":
+            print(checklist)
+        
+        case "U":
+            try:
+                index_input = int(input("Enter Item Index: "))
+                item_input = input("Enter Updated Item: ")
+                update(index_input, item_input)
+            except(ValueError, IndexError):
+                print("Invalid Index Input")
+        
+        case "D":
+            try:
+                print(destroy(int(input("Enter Index: "))))
+            except(ValueError, IndexError, TypeError):
+                print("Invalid Index Input")
+
+        case "Q":
+            return False
+        
+        case _:
+            print("Unknown Option\n")
+    return True
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(f"{index} {list_item} \n")
+        index += 1
+
+def mark_completed(index):
+    update(index, "√" + checklist[index])
+
+# this function is completely redundant, so I will just use input
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
 def test():
     create("purple sox")
     create("red cloak")
@@ -26,4 +78,12 @@ def test():
     print(read(0))
     # print(read(1))
 
-test()
+    list_all_items()
+    mark_completed(0)
+    list_all_items()
+
+
+running = True
+while running:
+    selection = input("Press C to add to list, R to read from list, P to display list, U to update list item, D to destroy item, and Q to quit: ")
+    running = select(selection)
